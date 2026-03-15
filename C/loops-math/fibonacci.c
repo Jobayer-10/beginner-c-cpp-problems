75951249525ecf9050aca98b353eff48540b2bff
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:21:41 +0600

    fibonacci.c

--- C/loops-math/fibonacci.c
+++ C/loops-math/fibonacci.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int n, a=0, b=1, next;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        printf("%d ",a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
