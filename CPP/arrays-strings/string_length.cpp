544efbabd131c3a5d1201136f65a5edc37ec0b7f
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:36:34 +0600

    Update string_length.cpp

--- CPP/arrays-strings/string_length.cpp
+++ CPP/arrays-strings/string_length.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    char str[100];
+    int length = 0;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    while(str[length] != '\0')
+        length++;
+
+    cout << "Length = " << length;
+
+    return 0;
+}
