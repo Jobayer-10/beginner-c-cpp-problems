8de282dfde8ee4ff6b3b2ed4e846c17dab5e0521
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:35:46 +0600

    Update average_array.cpp

--- CPP/arrays-strings/average_array.cpp
+++ CPP/arrays-strings/average_array.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int arr[100], n;
+    float sum = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Average = " << sum / n;
+
+    return 0;
+}
