d5c10c1cd0b0fcba1b800363f55ebef9d61d475d
Jobayer <prantojobayer308@gmail.com>
2026-03-15 17:41:11 +0600

    quadratic_roots.c

--- C/control-flow/quadratic_roots.c
+++ C/control-flow/quadratic_roots.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    float a,b,c,root1,root2,discriminant;
+
+    printf("Enter a, b, c: ");
+    scanf("%f %f %f",&a,&b,&c);
+
+    discriminant = b*b - 4*a*c;
+
+    if(discriminant > 0){
+        root1 = (-b + sqrt(discriminant)) / (2*a);
+        root2 = (-b - sqrt(discriminant)) / (2*a);
+        printf("Roots: %.2f and %.2f", root1, root2);
+    }
+    else if(discriminant == 0){
+        root1 = -b/(2*a);
+        printf("Equal Roots: %.2f", root1);
+    }
+    else{
+        printf("Complex Roots");
+    }
+
+    return 0;
+}
