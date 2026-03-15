85d06edb71ed1393edbd7021f7ed3e62d79eee25
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:25:19 +0600

    Update reverse_array.c

--- C/arrays-strings/reverse_array.c
+++ C/arrays-strings/reverse_array.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int arr[100], n;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    printf("Reversed array: ");
+
+    for(int i=n-1;i>=0;i--)
+        printf("%d ",arr[i]);
+
+    return 0;
+}
