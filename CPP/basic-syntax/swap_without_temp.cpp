d20b3032c837c06ebfb75d6d2e6ec376e2250781
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:30:39 +0600

    Update swap_without_temp.cpp

--- CPP/basic-syntax/swap_without_temp.cpp
+++ CPP/basic-syntax/swap_without_temp.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int a = 5, b = 10;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "After swap:" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
