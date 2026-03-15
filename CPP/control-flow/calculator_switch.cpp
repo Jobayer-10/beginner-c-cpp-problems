8532f90ad9cce46b06139fdc7006847563b18b6c
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:32:54 +0600

    Update calculator_switch.cpp

--- CPP/control-flow/calculator_switch.cpp
+++ CPP/control-flow/calculator_switch.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+ - * /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << "Result = " << num1 / num2;
+            break;
+
+        default:
+            cout << "Invalid operator";
+    }
+
+    return 0;
+}
