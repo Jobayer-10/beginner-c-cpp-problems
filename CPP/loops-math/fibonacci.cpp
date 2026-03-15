7896b5e58f208420f5643b3d0595d473b3a68389
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:33:19 +0600

    Update fibonacci.cpp

--- CPP/loops-math/fibonacci.cpp
+++ CPP/loops-math/fibonacci.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int n, first = 0, second = 1, next;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+
+        cout << first << " ";
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
