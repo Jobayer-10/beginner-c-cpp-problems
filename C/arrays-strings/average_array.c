76ec6b3fd2e5be00e7b62d14e91ff19a34f72fea
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:24:55 +0600

    Update average_array.c

--- C/arrays-strings/average_array.c
+++ C/arrays-strings/average_array.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int arr[100], n;
+    float sum=0;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+
+    printf("Average = %.2f", sum/n);
+
+    return 0;
+}
