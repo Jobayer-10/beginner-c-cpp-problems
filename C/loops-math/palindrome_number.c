cd39ce852c377bd72372fbaedfac23e9b23329f3
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:22:16 +0600

    Update palindrome_number.c

--- C/loops-math/palindrome_number.c
+++ C/loops-math/palindrome_number.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n, original, reverse=0, r;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    original=n;
+
+    while(n!=0){
+        r=n%10;
+        reverse=reverse*10+r;
+        n/=10;
+    }
+
+    if(original==reverse)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
