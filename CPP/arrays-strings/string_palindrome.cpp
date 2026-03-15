5615c0e8e8d83e0f24ebc9b0f65b5ab15cede7dc
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:37:02 +0600

    Update string_palindrome.cpp

--- CPP/arrays-strings/string_palindrome.cpp
+++ CPP/arrays-strings/string_palindrome.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main() {
+
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int len = strlen(str);
+    bool palindrome = true;
+
+    for(int i = 0; i < len/2; i++) {
+
+        if(str[i] != str[len-i-1]) {
+            palindrome = false;
+            break;
+        }
+    }
+
+    if(palindrome)
+        cout << "Palindrome string";
+    else
+        cout << "Not a palindrome string";
+
+    return 0;
+}
