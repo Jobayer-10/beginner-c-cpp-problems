4fec4e51348f26912784e2d07258a71acd5f9c00
Jobayer <prantojobayer308@gmail.com>
2026-03-16 02:30:59 +0600

    Update quotient_remainder.cpp

--- CPP/basic-syntax/quotient_remainder.cpp
+++ CPP/basic-syntax/quotient_remainder.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Quotient = " << a / b << endl;
+    cout << "Remainder = " << a % b << endl;
+
+    return 0;
+}
